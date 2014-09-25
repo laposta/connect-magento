9b417cf385e453fc38f1443fd5abb187ebc677e0
Merten van Gerven <merten.vg@gmail.com>
2014-09-25 16:18:49 +0200

    native subscriber id column added to laposta subscriber table

---
+++ public/app/code/community/Laposta/Connect/sql/lapostaconnect_setup/mysql4-install-1.0.12.php
@@ -0,0 +1,59 @@
+<?php
+
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->run(
+    "
+    CREATE TABLE IF NOT EXISTS `laposta_subscriber` (
+      `subscriber_id` int(11) unsigned NOT NULL auto_increment,
+      `list_id` int(11) unsigned NOT NULL default 1,
+      `customer_id` varchar(255) NOT NULL default '',
+      `newsletter_subscriber_id` varchar(255) DEFAULT NULL,
+      `laposta_id` varchar(255) NOT NULL default '',
+      `updated_time` datetime NULL,
+      `sync_time` datetime NULL,
+      PRIMARY KEY (`subscriber_id`),
+      INDEX `list_id` (`list_id`),
+      INDEX `customer_id` (`customer_id`),
+      INDEX `laposta_id` (`laposta_id`),
+      INDEX `newsletter_subscriber_id` (`newsletter_subscriber_id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+    "
+);
+
+$installer->run(
+    "
+    CREATE TABLE IF NOT EXISTS `laposta_field` (
+      `field_id` int(11) unsigned NOT NULL auto_increment,
+      `list_id` int(11) unsigned NOT NULL default 1,
+      `field_name` varchar(255) NOT NULL default '',
+      `field_relation` varchar(255) NOT NULL default '',
+      `laposta_id` varchar(255) NOT NULL default '',
+      `laposta_tag` varchar(255) NOT NULL default '',
+      `updated_time` datetime NULL,
+      `sync_time` datetime NULL,
+      PRIMARY KEY (`field_id`),
+      INDEX `list_id` (`list_id`),
+      INDEX `laposta_id` (`laposta_id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+    "
+);
+
+$installer->run(
+    "
+    CREATE TABLE IF NOT EXISTS `laposta_list` (
+      `list_id` int(11) unsigned NOT NULL auto_increment,
+      `list_name` varchar(255) NOT NULL default '',
+      `laposta_id` varchar(255) NOT NULL default '',
+      `webhook_token` varchar(255) NOT NULL default '',
+      `updated_time` datetime NULL,
+      `sync_time` datetime NULL,
+      PRIMARY KEY (`list_id`),
+      INDEX `webhook_token` (`webhook_token`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+    "
+);
+
+$installer->endSetup();
---
+++ public/app/code/community/Laposta/Connect/sql/lapostaconnect_setup/mysql4-upgrade-1.0.11-1.0.12.php
@@ -0,0 +1,13 @@
+<?php
+
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->run("
+ALTER TABLE `laposta_subscriber` ADD COLUMN `newsletter_subscriber_id` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `customer_id`
+");
+
+$installer->run("
+ALTER TABLE `laposta_subscriber` ADD INDEX `newsletter_subscriber_id` (`newsletter_subscriber_id`)
+");
