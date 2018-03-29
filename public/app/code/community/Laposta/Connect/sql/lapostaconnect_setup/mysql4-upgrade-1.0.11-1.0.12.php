ec9064d4b0a782ccdaaf3bfdc8a903c3f026b47d
Lars <webmaster@superdrukker.nl>
2018-03-29 11:30:56 +0200

    Bug fix: Prefix used in installer

--- public/app/code/community/Laposta/Connect/sql/lapostaconnect_setup/mysql4-upgrade-1.0.11-1.0.12.php
+++ public/app/code/community/Laposta/Connect/sql/lapostaconnect_setup/mysql4-upgrade-1.0.11-1.0.12.php
@@ -5,9 +5,9 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-ALTER TABLE `laposta_subscriber` ADD COLUMN `newsletter_subscriber_id` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `customer_id`
+ALTER TABLE `{$this->getTable('laposta_subscriber')}` ADD COLUMN `newsletter_subscriber_id` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `customer_id`
 ");
 
 $installer->run("
-ALTER TABLE `laposta_subscriber` ADD INDEX `newsletter_subscriber_id` (`newsletter_subscriber_id`)
+ALTER TABLE `{$this->getTable('laposta_subscriber')}` ADD INDEX `newsletter_subscriber_id` (`newsletter_subscriber_id`)
 ");
